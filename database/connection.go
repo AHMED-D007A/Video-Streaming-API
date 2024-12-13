3ce1b64e42aeeaeff12297227bfca63818d932c7
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-12-13 13:47:00 +0200

    database connection established

--- database/connection.go
+++ database/connection.go
@@ -1,1 +1,22 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func NewDBConnection(connStr string) *sql.DB {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return db
+}
--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "Video-Streaming-Platform/server"
+import (
+	"Video-Streaming-Platform/config"
+	"Video-Streaming-Platform/database"
+	"Video-Streaming-Platform/server"
+	"fmt"
+)
 
 func main() {
+	connStr := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable",
+		config.Envs.DB_HOST, config.Envs.DB_PORT, config.Envs.DB_USERNAME,
+		config.Envs.DB_PASSWORD, config.Envs.DB_NAME)
+	db := database.NewDBConnection(connStr)
+	defer db.Close()
+
 	server := server.NewServer(":8080", nil)
 
 	if err := server.Start(); err != nil {
